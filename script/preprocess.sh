71dbd16ae3cf46a8f1b3aa92647ba661c6301ef0
Eddie Ungless <s2112744@sms.ed.ac.uk>
2022-03-03 16:42:27 +0000

    attempt 2

--- script/preprocess.sh
+++ script/preprocess.sh
@@ -1,37 +1,60 @@
-#!/bin//sh
-#SBATCH -N 1      # nodes requested
-#SBATCH -n 1      # tasks requested
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Teach-Standard
+#SBATCH --nodelist=landonia07
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
+
 export CUDNN_HOME=/opt/cuDNN-7.0/
+
 export STUDENT_ID=$(whoami)
+
 export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
 export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
 export CPATH=${CUDNN_HOME}/include:$CPATH
+
 export PATH=${CUDA_HOME}/bin:${PATH}
+
 export PYTHON_PATH=$PATH
+
 mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TRANSFORMERS_OFFLINE = 1
 
 export TMPDIR=/disk/scratch/${STUDENT_ID}/
 export TMP=/disk/scratch/${STUDENT_ID}/
 
-source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+src_path=/home/s2112744/context-debias/data
 
-model_type=$1
-data=$2
-seed=42
+rsync --archive --update --compress --progress ${src_path}/ ${TMP}
+
+echo "Data transferred" 
+
+#src_path=/home/s2112744/context-debias/transformers
+
+#rsync --archive --update --compress --progress ${src_path}/ ${TMP}
+
+
+model_type=bert
+data=news-commentary-v15.en
+seed=30
 block_size=128
-
-# export TRANSFORMERS_OFFLINE=1	
-
 OUTPUT_DIR=../preprocess/$seed/$model_type
+
 rm -r $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-#python ../src/test.py
+python -u ../src/preprocess.py --input ../data/$data \
+                        --stereotypes ../data/cm_stereotype.txt \
+                        --attributes ../data/black.txt,../data/white.txt \
+                        --output $OUTPUT_DIR \
+                        --seed $seed \
+                        --block_size $block_size \
+                        --model_type $model_type
 
-python ../src/preprocess.py --input ../data/$data --stereotypes ../data/stereotype.txt --attributes ../data/female.txt,../data/male.txt --output $OUTPUT_DIR --seed $seed --block_size $block_size --model_type $model_type
